3cdac962266f50cf49019b445649c182f9fdefcb
NEOCORE\hadaa <hada.adam@gmail.com>
2024-05-02 13:39:06 +0200

    gitignore

--- graph.py
+++ graph.py
@@ -3,7 +3,10 @@ import main as stat
 from datetime import datetime
 import pandas as pd
 
-stat.initialize("mongodb://localhost:27017/","statdb")
+from pymongo import MongoClient
+client = MongoClient("mongodb://localhost:27017/")
+stat.initialize_connection(client,"statdb")
+
 TestStat = stat.EventStat('test')
 keys,values = TestStat.get_data_view(stat.EventInterval.MINUTE,start_date=datetime(2024, 4, 30, 13, 30),end_date=datetime(2024, 4, 30, 15, 30))
 
